4c3bedad12244c4b8fade3b05b8361b6eff8936c
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-04-03 19:16:55 +0400

    add a border to the text on the home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,10 +8,25 @@
 		display: block;
 		margin: 0 auto;
 		text-align: center;
+		display: grid;
+		place-items: center;
+		padding: 10px;
 	}
 	p {
 		font-size: 1.2rem;
 		line-height: 20px;
 		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+		max-width: 420px;
+		border: 1px solid #ccc;
+		padding: 20px;
+		border-radius: 4px;
+	}
+
+	a {
+		border: 1px solid rgba(33, 66, 336, 0.8);
+		padding: 5px 20px;
+		border-radius: 4px;
+		color: rgb(33, 66, 336);
 	}
 </style>
