246c0f7608240e824eaf49ee941286b9c4eed19d
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-04-03 19:02:11 +0400

    add ogp with local placeholder image

--- src/routes/[path]/__layout.reset.svelte
+++ src/routes/[path]/__layout.reset.svelte
@@ -2,23 +2,38 @@
 	import type { Load } from '@sveltejs/kit';
 
 	export const load: Load = async ({ fetch, url, params }) => {
-		const data = await fetch(`/data.json`, {
+		const meta = await fetch(`/data.json`, {
 			method: 'POST',
 			body: JSON.stringify({ pathname: params.path, query: url.search })
 		}).then((r) => r.json());
 
-		return { props: { data } };
+		return { props: { meta }, stuff: { meta } };
 	};
 </script>
 
 <script lang="ts">
-	import type { JSON_DATA } from '../types';
+	import type { JSON_DATA } from '$lib/types';
 
-	export let data: JSON_DATA;
+	export let meta: JSON_DATA;
+	const title = `${meta.reference}: ${meta.translation_name}`;
 </script>
 
-<div>
-	{JSON.stringify(data, null, 2)}
-</div>
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={meta.text} />
+	<meta name="og:site_name" content="Bible Links" />
+	<meta name="og:type" content="image" />
+
+	<meta name="og:title" content={title} />
+	<meta name="og:description" content={meta.text} />
+	<meta name="og:url" content="https://bible-api.com" />
+	<meta property="og:image" content="/static/good_news_bible.png" />
+
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={meta.text} />
+	<meta property="twitter:image" content="/static/good_news_bible.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@ChukwumaNwaugha" />
+</svelte:head>
 
 <slot />
--- src/routes/[path]/index.svelte
+++ src/routes/[path]/index.svelte
@@ -1,1 +1,10 @@
-<slot />
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div style="display: block; height: 100%; width: 100%">
+	{JSON.stringify($page.stuff.meta.text, null, 2)}
+	<!-- <p>
+		<img src="/static/good_news_bible.png" alt="Goodnews bible" />
+	</p> -->
+</div>
