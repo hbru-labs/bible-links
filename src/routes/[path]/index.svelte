679d80bf4e38f9b424d832b86a087dc31dc2c46b
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-04-09 15:48:11 +0400

    relayout the links page

--- src/routes/[path]/index.svelte
+++ src/routes/[path]/index.svelte
@@ -3,14 +3,21 @@
 </script>
 
 <div class="block-container">
+	<div class="image-wrapper">
+		<img width="240" src="logo.png" alt="Goodnews Bible" />
+	</div>
+
 	<div class="text-wrapper">
+		<p class="text text-20 text-bold reference">
+			{$page.stuff.meta.reference}
+		</p>
 		<span class="text text-20">
 			{$page.stuff.meta.text}
 		</span>
-	</div>
 
-	<div>
-		<img src="good_news_bible.png" alt="Goodnews Bible" />
+		<p class="text footer">
+			Powered by <a href="https://bible-api.com">https://bible-api.com</a>
+		</p>
 	</div>
 </div>
 
@@ -22,8 +29,28 @@
 		width: auto;
 		padding: 20px;
 	}
+	.image-wrapper {
+		display: flex;
+	}
+	.image-wrapper img {
+		object-fit: cover;
+		border-radius: 8px;
+		margin: 0 auto;
+	}
 	.text-wrapper {
-		border: 1px solid #ccc;
-		border-radius: 4px;
+		border: 1px solid #333;
+		border-radius: 8px;
+		margin-top: 40px;
+	}
+
+	.reference {
+		text-decoration: underline;
+		margin-bottom: 8px;
+	}
+
+	.footer {
+		margin-top: 20px;
+		text-align: left;
+		font-size: 10px;
 	}
 </style>
