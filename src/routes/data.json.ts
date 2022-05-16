1e38373118d6ad262d545ee520d6114d738d3a47
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-05-16 08:40:29 +0400

    dynamically import redis client in data.json

--- src/routes/data.json.ts
+++ src/routes/data.json.ts
@@ -1,26 +1,35 @@
 import type { RequestHandler } from '@sveltejs/kit';
-import { client } from '$lib/services/redis';
+import type { Redis } from 'ioredis';
 
 const BASE_URL = 'https://bible-api.com';
 
 export const post: RequestHandler = async ({ request }) => {
 	const req = await request.json();
 	const key = `${req.pathname}-${req.query || ''}`;
-	// check whether request was cached
-	const cached = await client.get(key);
-	if (cached) {
-		return {
-			body: {
-				...JSON.parse(cached),
-				cached: true
-			}
-		};
+	let redisClient: Redis | null = null;
+
+	try {
+		const { client } = await import('$lib/services/redis');
+		// check whether request was cached
+		const cached = await client.get(key);
+		if (cached) {
+			return {
+				body: {
+					...JSON.parse(cached),
+					cached: true
+				}
+			};
+		}
+
+		redisClient = client;
+	} catch (error) {
+		console.log('E: reading from redis', error);
 	}
 
 	const url = `${BASE_URL}/${req.pathname}${req.query}`;
 	const result = await fetch(url).then((r) => r.json());
 	// cache result
-	await client.set(key, JSON.stringify(result));
+	await redisClient?.set(key, JSON.stringify(result));
 
 	return {
 		body: result
