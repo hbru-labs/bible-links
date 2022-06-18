4678623cf0781786a06cec9a4900ca750a72da2e
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-06-18 21:04:25 +0200

    create a footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,17 @@
+<div
+	class="block mx-auto my-0 w-auto p-2.5 max-w-[420px] text-md bottom-0 fixed text-left mt-5 left-1/2 footer"
+>
+	Powered by <a href="https://bible-api.com">https://bible-api.com</a>
+</div>
+
+<style>
+	.footer {
+		font-size: 10px;
+		transform: translateX(-50%);
+	}
+
+	a:link {
+		color: blue;
+		text-decoration: underline;
+	}
+</style>
