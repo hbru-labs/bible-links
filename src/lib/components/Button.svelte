aef557b704969a735a71cae565b6b75833a84a07
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-06-18 09:10:16 +0200

    create a button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let disabled: boolean = false;
+</script>
+
+<button
+	on:click
+	{disabled}
+	class="inline-block w-full text-center px-4 py-2 border border-indigo-500 text-black bg-white hover:bg-zinc-50 transition-all ease-in-out rounded-md disabled:opacity-75 disabled:border-zinc-400 disabled:bg-zinc-50"
+>
+	<slot>Search</slot>
+</button>
