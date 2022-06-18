6f0948aaee0af78dd9b2eda77a01428a0307a17b
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-06-18 09:10:33 +0200

    create a searchbar component

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let searchTerm = '';
+	export let autofocus = true;
+
+	let inputEl: HTMLInputElement | undefined;
+	onMount(() => {
+		autofocus && inputEl?.focus();
+	});
+</script>
+
+<search-bar>
+	<div
+		class="flex h-10 w-full items-center space-x-2 rounded-md border-2 border-zinc-500 py-2 px-2.5 focus-within:border-indigo-600"
+	>
+		<div class="my-2 flex h-7 w-7 items-center justify-center">
+			<svg
+				fill="#000000"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 30 30"
+				width="30px"
+				height="30px"
+				><path
+					d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"
+				/></svg
+			>
+		</div>
+		<input
+			type="search"
+			class="text-md block h-6 w-full border-none bg-transparent pl-0 outline-none focus:ring-0"
+			placeholder="Start typing to search..."
+			bind:value={searchTerm}
+			bind:this={inputEl}
+		/>
+	</div>
+</search-bar>
+
+<style>
+	input[type='search']::-webkit-search-cancel-button {
+		display: none;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,30 @@
-<!-- Make this a search page -->
+<script lang="ts">
+	import Search from '$lib/components/Search.svelte';
+	import Logo from '$lib/components/Logo.svelte';
+	import Button from '$lib/components/Button.svelte';
+
+	let searchTerm = '';
+
+	async function search() {
+		console.log({ searchTerm });
+	}
+</script>
+
 <div class="grid-container">
-	<p class="text text-20">Enter a valid Bible link, or go to the following link for usage guide</p>
-	<a href="https://bible-api.com">Powered by https://bible-api.com</a>
+	<Logo size="medium" />
+
+	<div class="grid-x grid-margin-x">
+		<div class="flex flex-col space-y-3 items-center">
+			<Search bind:searchTerm />
+			<div class="w-3/4">
+				<Button on:click={search} disabled={!searchTerm} />
+			</div>
+		</div>
+	</div>
+
+	<div class="text footer">
+		Powered by <a href="https://bible-api.com">https://bible-api.com</a>
+	</div>
 </div>
 
 <style>
@@ -20,11 +43,21 @@
 		border-radius: 4px;
 	}
 
-	a {
-		border: 1px solid rgba(33, 66, 336, 0.8);
-		padding: 5px 20px;
-		border-radius: 4px;
-		color: rgb(33, 66, 336);
-		text-decoration: none;
+	div.footer {
+		display: block;
+		margin: 0 auto;
+		max-width: 420px;
+		width: auto;
+		padding: 20px;
+	}
+
+	.footer {
+		margin-top: 20px;
+		text-align: left;
+		font-size: 10px;
+		position: fixed;
+		bottom: 0;
+		left: 50%;
+		transform: translateX(-50%);
 	}
 </style>
