11fc1241a1cb14cab8aacd487c4b12d24399f91f
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-07-13 00:22:18 +0100

    add a select component

---
+++ src/lib/components/Select.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" context="module">
+	export interface SelectType {
+		id: string;
+		label: string;
+	}
+</script>
+
+<script lang="ts">
+	export let options: SelectType[];
+	export let value: string;
+</script>
+
+<container class="inline-block">
+	<slot name="label">
+		<label for="select">Select:</label>
+	</slot>
+	<select name="select" id="select" on:change {value}>
+		{#each options as option (option.id)}
+			<option value={option.id}>{option.label}</option>
+		{:else}
+			<option value="empty">Empty</option>
+		{/each}
+	</select>
+</container>
