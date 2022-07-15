061e360001dabb588cebd68825794c912fe01b49
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-07-15 23:31:42 +0100

    allow ssr in redis hook only when deployed

--- src/hooks/redis.ts
+++ src/hooks/redis.ts
@@ -2,6 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import { prerendering } from '$app/env';
 
 // would be used for SSR
+const prod = process.env.NODE_ENV === 'production';
 const redis: Handle = async ({ event, resolve }) => {
 	if (/(\/_ah\/redis\/?)$/.test(event.url.pathname) && !prerendering) {
 		const { redis } = await import('$lib/services/redis');
@@ -19,7 +20,7 @@ const redis: Handle = async ({ event, resolve }) => {
 		return Response.redirect(event.url.origin + '/', 303);
 	}
 
-	const response = await resolve(event, { ssr: true });
+	const response = await resolve(event, { ssr: prod });
 	return response;
 };
 
--- src/routes/api/redis-check.ts
+++ src/routes/api/redis-check.ts
@@ -3,9 +3,14 @@ import type { RequestHandler } from '@sveltejs/kit';
 export const get: RequestHandler = async function ({ url }) {
 	try {
 		return {
-			body: { redis: (await fetch(`${url.origin}/_ah/redis`)).statusText }
+			body: {
+				redis: (await fetch(`${url.origin}/_ah/redis`)).statusText
+			}
 		};
 	} catch (error) {
-		return { status: 500, body: error };
+		return {
+			status: 500,
+			body: error
+		};
 	}
 };
