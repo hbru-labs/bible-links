ba4f521cebc38be902fc69c1c2ca16011fa47f4a
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-07-17 16:00:18 +0100

    bring back sentry in the project

---
+++ src/lib/services/sentryBrowser.ts
@@ -0,0 +1,14 @@
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+	dsn: 'https://6c1adb024c1a44c79abffe08280eb3af@o1321256.ingest.sentry.io/6577924',
+	integrations: [new BrowserTracing()],
+	tracesSampleRate: 1.0
+});
+
+Sentry.setContext('sentry-browser', {
+	date: new Date()
+});
+
+export const captureException = Sentry.captureException;
