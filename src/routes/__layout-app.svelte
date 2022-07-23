41e528fd07a7c4d671bc7c192fa599191cc48469
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-07-23 09:19:57 +0100

    revert the last commit

--- src/routes/__layout-app.svelte
+++ src/routes/__layout-app.svelte
@@ -2,7 +2,35 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Logo from '$lib/components/Logo.svelte';
 	import { page } from '$app/stores';
+
+	const title = 'Bible Links';
+	const imgSrc =
+		'https://res.cloudinary.com/cpnwaugha/image/upload/v1649503686/bible-links/main_logo.png';
+	const description =
+		'Share beautiful Bible verses and chapters on Social Media with the goal of supporting communities to remain strong in Christ';
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<meta name="og:title" content={title} />
+	<meta name="og:description" content={description} />
+	<meta name="og:site_name" content="Bible Links" />
+	<meta name="og:type" content="website" />
+	<meta name="og:url" content={$page.url.href} />
+
+	<meta property="og:image" content={imgSrc} />
+	<meta property="og:image:alt" content="${title}" />
+	<meta property="og:image:width" content="720" />
+	<meta property="og:image:height" content="540" />
+
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta property="twitter:image" content={imgSrc} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@ChukwumaNwaugha" />
+</svelte:head>
 
 <slot />
 
--- src/routes/index@app.svelte
+++ src/routes/index@app.svelte
@@ -3,41 +3,13 @@
 	import Logo from '$lib/components/Logo.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
 
 	let searchTerm = '';
-	const title = 'Bible Links';
-	const imgSrc =
-		'https://res.cloudinary.com/cpnwaugha/image/upload/v1649503686/bible-links/main_logo.png';
-	const description =
-		'Share beautiful Bible verses and chapters on Social Media with the goal of supporting communities to remain strong in Christ';
 
 	function navigateToSearch() {
 		goto('/search?q=' + encodeURIComponent(searchTerm));
 	}
 </script>
-
-<svelte:head>
-	<title>{title}</title>
-	<meta name="description" content={description} />
-
-	<meta name="og:title" content={title} />
-	<meta name="og:description" content={description} />
-	<meta name="og:site_name" content="Bible Links" />
-	<meta name="og:type" content="website" />
-	<meta name="og:url" content={$page.url.href} />
-
-	<meta property="og:image" content={imgSrc} />
-	<meta property="og:image:alt" content="${title}" />
-	<meta property="og:image:width" content="720" />
-	<meta property="og:image:height" content="540" />
-
-	<meta name="twitter:title" content={title} />
-	<meta name="twitter:description" content={description} />
-	<meta property="twitter:image" content={imgSrc} />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="@ChukwumaNwaugha" />
-</svelte:head>
 
 <div class="home h-screen">
 	<div class="grid place-items-center p-2.5 text-center mx-auto my-0 gap-4">
