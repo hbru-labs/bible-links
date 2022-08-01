12ceb1550fbb8c1d4fb3ab2187c103f241bd7a9b
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-08-01 04:35:15 +0100

    add more function abstraction test

--- tests/sanitizePuntuations.ava.ts
+++ tests/sanitizePuntuations.ava.ts
@@ -8,3 +8,19 @@ test('correctly sanitizes punctuation between 2 words', (t) => {
 	t.is(sanitizePunctuations('hello?world!'), 'hello? world!');
 	t.is(sanitizePunctuations('hello!world!'), 'hello! world!');
 });
+
+test('only sanitizes on defined punctuations', (t) => {
+	t.is(sanitizePunctuations('hello@world!'), 'hello@world!');
+	t.is(sanitizePunctuations('hello#world!'), 'hello#world!');
+	t.is(sanitizePunctuations('hello&world!'), 'hello&world!');
+	t.is(sanitizePunctuations('hello$world!'), 'hello$world!');
+	t.is(sanitizePunctuations('hello%world!'), 'hello%world!');
+});
+
+test('does not sanitize punctuation between 2 non-words', (t) => {
+	t.is(sanitizePunctuations('hello,,world!'), 'hello,,world!');
+	t.is(sanitizePunctuations('hello;-world!'), 'hello;-world!');
+	t.is(sanitizePunctuations('hello.&world!'), 'hello.&world!');
+	t.is(sanitizePunctuations('hello?$world!'), 'hello?$world!');
+	t.is(sanitizePunctuations('hello!#world!'), 'hello!#world!');
+});
