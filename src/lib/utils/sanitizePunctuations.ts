582f31dc45655bf282142ef68b1830848f92907b
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-08-05 20:59:32 +0100

    construct paragraph using new line for supported translations

--- src/lib/utils/sanitizePunctuations.ts
+++
@@ -1,4 +0,0 @@
-export default function sanitizePunctuations(text: string) {
-	// add appropriate space between words having punctuations using regex
-	return text.replace(/([a-zA-Z_][\w])([?!.;,])([\w])/g, '$1$2 $3');
-}
