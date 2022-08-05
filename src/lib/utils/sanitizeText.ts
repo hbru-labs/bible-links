a433edff40d1d5546632e23db7d4ec14cb5a1c2d
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-08-05 21:08:09 +0100

    refactor sanitize-text; update sanitize-text.ava

--- src/lib/utils/sanitizeText.ts
+++ src/lib/utils/sanitizeText.ts
@@ -1,16 +1,18 @@
-const HAS_NEW_LINE = ['web', 'oeb-cw', 'oeb-us'];
+export const HAS_NEW_LINE = ['web', 'oeb-cw', 'oeb-us'];
 
 export function sanitizePunctuations(text: string) {
 	// add appropriate space between words having punctuations using regex
 	return text.replace(/([a-zA-Z_][\w])([?!.;,])([\w])/g, '$1$2 $3');
 }
 
-export function constructParagraphs(text: string) {
-	return text
-		.split('\n')
+export function uppercaseFirstChar(paragraphs: string[]) {
+	return paragraphs
 		.filter((paragraph) => paragraph.length > 0)
-		.map((line) => line.replace(/\b\w/, (char) => char.toUpperCase()))
-		.join('\n\n');
+		.map((line) => line.replace(/\b\w/, (char) => char.toUpperCase()));
+}
+
+export function constructParagraphs(text: string) {
+	return uppercaseFirstChar(text.split('\n')).join('\n\n');
 }
 
 export default function sanitizeText(text: string, tr: string) {
