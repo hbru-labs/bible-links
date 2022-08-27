b3c793025495a3eea9393ddf4a08ec6b9437e76d
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-08-27 18:14:38 +0100

    add book-search to home page

--- src/routes/index@app.svelte
+++ src/routes/index@app.svelte
@@ -25,13 +25,18 @@
 		<div class="flex justify-center mb-4 w-3/5">
 			<div class="flex flex-col space-y-3 items-center max-w-[480px]">
 				<div class="text-lg lowercase leading-4 mb-3 font-semibold">Search the Bible</div>
-				<SearchBar
-					bind:searchTerm
-					on:keypress={navigateToSearch}
-					onclickPress={() => modalEl.openModal()}
-				/>
+				<SearchBar bind:searchTerm on:keypress={navigateToSearch} />
 				<div class="w-[140px]">
-					<Button on:click={navigateToSearch} disabled={!searchTerm} />
+					<Button on:click={navigateToSearch} disabled={!searchTerm} label="Search" />
+				</div>
+
+				<div class="w-full pt-0 flex flex-col items-center" style="margin-top: 60px;">
+					<hr class="w-full h-1 mb-2" />
+					<div class="flex justify-center cursor-pointer p-3">
+						<span class="text-sm text-indigo-500" on:click={() => modalEl.openModal()}
+							>Search using book+chapter:verse</span
+						>
+					</div>
 				</div>
 			</div>
 		</div>
