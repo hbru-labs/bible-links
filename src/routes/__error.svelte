61ccd932be1dea1d90834fbffec846cd15c01eb1
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-08-31 14:46:28 +0100

    add book-search-modal to error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -11,30 +11,39 @@
 			props: { message: error.message, currentTranslation }
 		};
 	};
+
+	const PROD_ENV = process.env.NODE_ENV === 'production';
 </script>
 
 <script lang="ts">
 	import OtherTranslations from '$lib/components/OtherTranslations.svelte';
+	import BookSearchModal from '$lib/components/BookSearchModal.svelte';
 
 	export let message: string;
 	export let currentTranslation: string;
+
+	let modalEl: BookSearchModal;
 </script>
 
-<div class="root-error">
-	<p>Uh-Oh: {message}</p>
+<div class="root-error flex flex-col justify-center items-center p-5">
+	<p class="font-bold" style="font-size: 16px;">
+		Uh-Oh: something went wrong
+		{#if !PROD_ENV}
+			<span class="block font-normal text-sm">
+				{message}
+			</span>
+		{/if}
+	</p>
 	<OtherTranslations {currentTranslation} />
-</div>
 
-<style>
-	.root-error {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		padding: 20px;
-	}
-	.root-error p {
-		font-size: 16px;
-		font-weight: bold;
-	}
-</style>
+	<div class="w-4/5 sm:w-2/5 pt-0 flex flex-col items-center" style="margin-top: 60px;">
+		<hr class="w-full h-1 mb-2" />
+		<div class="flex justify-center cursor-pointer p-3">
+			<span class="text-sm text-indigo-500" on:click={() => modalEl.openModal()}
+				>Search using book+chapter:verse</span
+			>
+		</div>
+	</div>
+
+	<BookSearchModal bind:this={modalEl} />
+</div>
