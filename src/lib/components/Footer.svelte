da9f25de87f30b8246371b88118a73a1305e4cca
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-09-10 05:03:12 +0100

    open footer links in new tab

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,8 @@
 <div
 	class="block mx-auto my-0 w-auto p-2.5 max-w-[420px] text-md bottom-0 fixed text-left mt-5 left-1/2 footer"
 >
-	Powered by <a href="https://bible-api.com">https://bible-api.com</a>, Twitter: <a href="https://twitter.com/bible_links">@bible_links</a>
+	Powered by <a href="https://bible-api.com" target="_blank">https://bible-api.com</a>, Twitter:
+	<a href="https://twitter.com/bible_links" target="_blank">@bible_links</a>
 </div>
 
 <style>
