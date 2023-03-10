9bdaad3e096b6b30380c925524645f66a3923a10
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-03-10 03:32:18 -0800

    add ask-the-bible endpoint for question answering

--- src/lib/services/openai.ts
+++ src/lib/services/openai.ts
@@ -2,7 +2,6 @@ import { getSecret } from '../utils/secret';
 import { Configuration, OpenAIApi, type CreateCompletionResponse } from 'openai';
 import logger from '../utils/logger';
 import cosineSimilarity from '../utils/cosineSimilarity';
-import { captureException } from '@sentry/node';
 
 let client: OpenAIApi | null = null;
 
@@ -28,6 +27,10 @@ const systemMessages = {
 	bibleScholar:
 		'You are a Bible Scholar, Doctor of the Church, Dominican Order Priest with PhDs in Theology and Philosophy. You can explain Bible concepts in great details with simplicity and finesse, to the understanding of any audience.'
 };
+const testMessages = {
+	user: `How does the Bible explain God's love for us?`,
+	assistant: `The Bible explains God's love for us as sacrificial, unconditional, and everlasting. It is a love that surpasses all human understanding and is demonstrated through the death of Jesus Christ on the cross. <a class="underline text-blue-500" href="https://bible-links.vercel.app/John3:16">John 3:16</a> says, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life." This shows that God's love for us is not based on our merit or worthiness, but solely on His grace and mercy towards us.`
+};
 
 export function getAPI(openai: OpenAIApi) {
 	return logger.logMethodCalls({
@@ -48,25 +51,6 @@ export function getAPI(openai: OpenAIApi) {
 				});
 
 			return response.data.error ? '' : (response.data as CreateCompletionResponse).choices[0].text;
-		},
-		async textSummarizationDeprecated(text: string) {
-			const prompt = `I want you to act as a Bible Scholar, Doctor of the Church, Dominican Order Priest with PhDs in Theology and Philosophy. You will explain Bible concepts in Great details with simplicity and finesse to the understanding of any audience. You will ensure to preserve the truth in the Gospel and teach hidden meanings not readily apparent to everyday users. When prompted you'll provide a summary with explanation in great detail. Now, summarize the following bible text: \n\n${text}`;
-			const response = await openai
-				.createCompletion({
-					model: 'text-davinci-003',
-					prompt,
-					temperature: 0.5,
-					max_tokens: 1024,
-					top_p: 1.0,
-					frequency_penalty: 0.0,
-					presence_penalty: 1
-				})
-				.catch((error) => {
-					captureException(error);
-					throw error;
-				});
-
-			return (response.data as CreateCompletionResponse).choices[0].text;
 		},
 		async textSummarization(text: string) {
 			const prompt = `I want you to act as a Bible Scholar, Doctor of the Church, Dominican Order Priest with PhDs in Theology and Philosophy. You can explain Bible concepts in Great details with simplicity and finesse to the understanding of any audience. You will ensure to preserve the truth in the Gospel and teach hidden meanings not readily apparent to everyday users. When prompted you'll provide a summary with explanation in great detail. Now, summarize the following bible text keeping it brief and less than 144 words: \n\n${text}`;
@@ -93,15 +77,17 @@ export function getAPI(openai: OpenAIApi) {
 			You will ensure to preserve the truth in the Gospel and teach hidden meanings not readily apparent to everyday users. 
 			Your goal is to expand on the user's query or prompt and provide a relevant response. 
 			You will use your deep knowledge of word constructs in English to cater for poorly constructed query/prompt. 
-			You will also provide Bible references at the end to delight the user and encouraging further learning. 
-			The references should come in a list after your explanation. Each List item should be a link with the format <a href="https://bible-links.vercel.app/{book}{chapter}:{verseBegin}-{verseEnd}">{reference}</a>.
-			Now, provide an explaination to the following query about the bible: keep it brief and less than 75 words: \n\n${query}`;
+			You will provide relevant Bible references where necessary to delight the user and encourage further learning. 
+			You will ensure the references are a link with the format <a class="underline text-blue-500" href="https://bible-links.vercel.app/{book}{chapter}:{verseBegin}-{verseEnd}">{reference}</a>.
+			Now, provide an explaination to the following query about the bible: keep it brief and less than 79 words. \n\nQuery: ${query}`;
 			const response = await openai
 				.createChatCompletion(
 					{
 						model: 'gpt-3.5-turbo',
 						messages: [
 							{ role: 'system', content: systemMessages.bibleScholar },
+							{ role: 'user', content: testMessages.user },
+							{ role: 'assistant', content: testMessages.assistant },
 							{ role: 'user', content: prompt }
 						],
 						temperature: 0.5,
